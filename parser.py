83c9623b1ec998766a6009abc05439c0c4b07d8e
Shrubrio <90871281+Shrubrio@users.noreply.github.com>
2022-09-26 17:22:52 -0500

    adding the rest of what I have done so far
    giving you guys a chance to put some work in this, the only parts I need are the week by week and monthly basis. I have done the each day and set up the formatting for the dates themselves.

--- parser.py
+++ parser.py
@@ -1,12 +1,26 @@
-from stat import filemode
-import urllib.request
 from urllib.request import urlretrieve
+import urllib.request
 import os.path
 from os import path
+import re
+from collections import Counter
 
-weburl = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
-resultcode = print("result code: " + str(weburl.getcode()))
-    
+#variables
+url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+local = 'logfile.txt'
+dates = []
+Files = {}
+filenames = []
+errorcodes = []
+numdate = []
+num1dates = []
+yeartotal = []
+NumMonth = {}
+code4 = 0
+code3 = 0
+
+
+#pull marketing info and make log 
 if path.isfile('logfile.txt') == False:
     print('downloading log file...')
     url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
@@ -14,36 +28,71 @@ if path.isfile('logfile.txt') == False:
     print('done')
 else: print('this file is already present on your workstation')
     
-file=open('logfile.txt', 'r')
 
-Files = {}
-filename = []
-logfile = []
-dates = []
-NumDates = []
-Num1Dates = []
-yeartotal = []
-NumMonth = {}
-lineseg = []
+#open and get to work
+open_log = open('logfile.txt', 'r')
 
-code4 = 0
-code3 = 0
-requests = []
-
-
-
-percent3xx = (code3 / len(dates)) * 100
-percent3xx = "{:.2f}" .format(percent3xx)
-percent4xx = (code4 / len(dates)) * 100
-percent3xx = "{:.2f}" .format(percent4xx)
-
-request_total = 0
+for row in open_log: 
+    split = row.split(' ')
+    if(len(split) > 8):
+        errorcodes.append(split[8])
+        filenames.append(split[6])
+    if(len(split[3]) > 14):
+        dates.append(split[3]) 
+        
+for issues in errorcodes: 
+    if(issues[0] == '3'):
+        code3 = code3 + 1
+    if(issues[0] == '4'):
+        code4 = code4 + 1
     
-for line in open(r"logfile.txt", 'r'):
-    if "GET" in line:
-        request_total += 1
+code3percent = (code3 / len(dates)) * 100
+code3percent = "{:.2f}".format(code3percent)
+code4percent = (code4 / len(dates)) * 100
+code4percent = "{:.2f}".format(code4percent)
 
-print("Total number of requests:", request_total)
+for date in dates:
+    numdate.append(date[1:12])
+    num1dates.append(date[1:3])
 
-    
-fp.close()
+for d in num1dates:
+    if(d in NumMonth):
+        NumMonth[d] += 1
+    else:
+        NumMonth[d] = 1
+        
+for file in filenames:
+    if(file in Files):
+        Files[file] += 1
+    else:
+        Files[file] = 1      
+        
+most_requested = max(Files, key=Files.get)
+least_requested = min(Files, key=Files.get)
+
+print("How many requests were made on each day?")
+for a, b in sorted(NumMonth.items()):
+	print("{} : {}".format(a,b))
+print('----------------------------------------')
+
+print("How many requests were made on a week-by-week basis?")
+print()
+
+print("How many requests were made per month?")
+print()
+
+print("What percentage of requests resulted in a 4xx error code")
+print("{}%".format(code4percent))
+print('--------------------------------------------------------')
+
+print("What percentage of the requests resulted in a 3xx error code")
+print("{}%".format(code3percent))
+print('----------------------------------------------------------')
+
+print("What was the most-requested file?")
+print(most_requested)
+print('---------------------------------')
+
+print("What was the lease-requested file?")
+print(least_requested)
+print('---------------------------------')
